51840b797e3703696ab08711d26cb28b60b1874c
zhengzhanpeng <714586750@qq.com>
2018-03-13 17:01:17 +0800

    refactor OpenSearch.java and EntirelyOpenSearch.java

--- src/main/java/com/Albert/search/openSearch/EntirelyOpenSearch.java
+++ src/main/java/com/Albert/search/openSearch/EntirelyOpenSearch.java
@@ -2,15 +2,16 @@ package com.Albert.search.openSearch;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Albert
  * @create 2018-03-11 14:10
  */
-public interface EntirelyOpenSearch<PathT, KeyT, ResultT> extends OpenSearch<PathT, KeyT, ResultT> {
-    List<ResultT> getResultsUntilTimeout(PathT pathT, KeyT keyT, long timeout, TimeUnit unit);
-    List<ResultT> getResultsUntilOneTimeout(PathT pathT, KeyT keyT, long timeout, TimeUnit unit);
-    List<ResultT> getResultsUntilEnoughOrTimeout(PathT pathT, KeyT keyT, long timeout, TimeUnit unit, int exceptNum);
-    List<ResultT> getResultsUntilEnoughOrOneTimeout(PathT pathT, KeyT keyT, long timeout, TimeUnit unit, int exceptNum);
-    List<ResultT> getResultsUntilEnough(PathT pathT, KeyT keyT, int exceptNum);
+public interface EntirelyOpenSearch<KeyT, ResultT, PathT> extends OpenSearch<KeyT, ResultT, PathT> {
+    List<ResultT> getResultsUntilTimeout(List<PathT> pathList, KeyT keyT, long timeout, TimeUnit unit);
+    List<ResultT> getResultsUntilOneTimeout(List<PathT> pathList, KeyT keyT, long timeout, TimeUnit unit);
+    List<ResultT> getResultsUntilEnoughOrTimeout(List<PathT> pathList, KeyT keyT, long timeout, TimeUnit unit, int exceptNum);
+    List<ResultT> getResultsUntilEnoughOrOneTimeout(List<PathT> pathList, KeyT keyT, long timeout, TimeUnit unit, int exceptNum);
+    List<ResultT> getResultsUntilEnough(List<PathT> pathList, KeyT keyT, int exceptNum) throws TimeoutException;
 }
--- src/main/java/com/Albert/search/openSearch/OpenSearch.java
+++ src/main/java/com/Albert/search/openSearch/OpenSearch.java
@@ -1,11 +1,14 @@
 package com.Albert.search.openSearch;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Albert
  * @create 2018-03-11 13:39
  */
-public interface OpenSearch<PathT, KeyT, ResultT> {
-    ResultT getAResult(PathT pathT, KeyT keyT);
+public interface OpenSearch<KeyT, ResultT, PathT> {
+    ResultT getAResult(List<PathT> pathList, KeyT keyT);
 
-    ResultT getAResultUntilTimeout(PathT pathT, KeyT keyT);
+    ResultT getAResultUntilTimeout(List<PathT> pathList, KeyT keyT, long timeout, TimeUnit unit);
 }
