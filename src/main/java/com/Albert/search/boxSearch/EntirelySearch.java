299cf01b8ab11fafc9beec5bf10c2e947a1592fa
zhengzhanpeng <714586750@qq.com>
2018-03-13 16:03:38 +0800

    refactor Type from KeySearchT to KeyT in EntirelySearch.java

--- src/main/java/com/Albert/search/boxSearch/EntirelySearch.java
+++ src/main/java/com/Albert/search/boxSearch/EntirelySearch.java
@@ -8,10 +8,10 @@ import java.util.concurrent.TimeoutException;
  * @author Albert
  * @create 2018-02-08 16:53
  */
-public interface EntirelySearch<KeySearchT, ResultT> extends Search<KeySearchT, ResultT> {
-    List<ResultT> getResultsUntilOneTimeout(KeySearchT keySearchT, long timeout, TimeUnit unit);
-    List<ResultT> getResultsUntilTimeout(KeySearchT keySearchT, long timeout, TimeUnit unit);
-    List<ResultT> getResultsUntilEnoughOrTimeout(KeySearchT keySearchT, int expectNum, long timeout, TimeUnit unit);
-    List<ResultT> getResultsUntilEnoughOrOneTimeout(KeySearchT keySearchT, int expectNum, long timeout, TimeUnit unit);
-    List<ResultT> getResultsUntilEnough(KeySearchT keySearchT, int expectNum) throws TimeoutException;
+public interface EntirelySearch<KeyT, ResultT> extends Search<KeyT, ResultT> {
+    List<ResultT> getResultsUntilOneTimeout(KeyT keyT, long timeout, TimeUnit unit);
+    List<ResultT> getResultsUntilTimeout(KeyT keyT, long timeout, TimeUnit unit);
+    List<ResultT> getResultsUntilEnoughOrTimeout(KeyT keyT, int expectNum, long timeout, TimeUnit unit);
+    List<ResultT> getResultsUntilEnoughOrOneTimeout(KeyT keyT, int expectNum, long timeout, TimeUnit unit);
+    List<ResultT> getResultsUntilEnough(KeyT keyT, int expectNum) throws TimeoutException;
 }
